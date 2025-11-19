9e6d641a480e5d474f8b54e61ede3ba1bd3075e6
Astrophysics108 <dsingh@perse.co.uk>
2025-11-19 20:33:23 +0000

    conf for deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astrophysics108.github.io',
+  base: "/hackatamagotchi-site",
+})
